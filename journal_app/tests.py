0009bfe5dbb8787ec4b42fa8aebde5ec51902d45
Kotauror <zygmunt.justyna@gmail.com>
2022-03-17 08:17:05 +0100

    Change how test is run.

--- journal_app/tests.py
+++ journal_app/tests.py
@@ -1,7 +1,10 @@
-from django.test import TestCase
-from journal_app.models import Posts
+# from django.test import TestCase
+# from journal_app.models import Posts
+import unittest
 
-
-class SimpleTestCase(TestCase):
+class SimpleTestCase(unittest.TestCase):
     def test_that_number_is_returned(self):
-        self.assertEqual(Posts.num(3), 3)
+        self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()
